3525a8397f49b40cf9ce4c988dc8324f7335d4d2
橋本真奈美 <manako.075h@gmail.com>
2023-09-12 16:53:59 +0900

    no message

---
+++ develop/concept.php
@@ -0,0 +1,109 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "concept",
+  "gnav_set" => "concept",
+  "page_class" => "",
+  "url" => "concept.html",
+  "title" => "コンセプト｜【公式】シティタワー東別院｜東別院の新築マンション｜住友不動産",
+  "keywords" => "コンセプト,シティタワー東別院,東別院 マンション,東別院 新築マンション,東別院,東別院駅,地下鉄名城線,名古屋市中区,マンション,新築マンション,分譲マンション,住友不動産",
+  "description" => "コンセプトページ。地下鉄名城線「東別院駅」まで徒歩8分。住友不動産の新築マンションが東別院に誕生。【すみふ東別院】名古屋市中区のマンションなら住友不動産。",
+  "add_stylesheet" => ["css/concept.css"],
+  "add_script" =>  [],
+  
+  "logo_text" => "東別院駅徒歩8分｜シティタワー東別院｜東別院 新築マンション｜現地案内図｜すみふ東別院｜住友不動産"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー東別院｜東別院 マンション｜トップ</a></li>
+      <li><p>コンセプト</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/concept/mv.png" alt="" class="pc"><img src="./images/concept/mv-sp.png" alt="" class="sp"><p class="annotation white">航空写真</p></div>
+  </header><!-- main-visual -->
+
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <p class="text">江戸時代から、宿場町として賑わった登戸の街。<br>早くから成熟を果たし、人々が住み継いできたこの街が<br class="pc">いま、生まれ変わろうとしている。
+      <br>土地区画整理事業<sup>※1</sup>によって整備され、より安全で快適な街へ。<br>「新宿」駅へ直通20分<sup>※2</sup>、「大手町」駅へ34分<sup>※3</sup>。<br>都心近接でありながら、多摩川<sup>※4</sup>、生田緑地<sup>※5</sup>の豊かな自然環境と、<br class="pc">利便性をも享受する快適な住環境に、<br class="pc">モダンな美しさを放つレジデンスが誕生する。 </p>
+      </div>
+      <div class="image img01">
+        <img src="./images/concept/intro01.jpg" alt="" class="pc"><img src="./images/concept/intro01-sp.jpg" alt="" class="sp">
+        <p class="annotation white">image photo</p>
+      </div>
+      <ul class="clearfix">
+        <li>
+        <div class="image">
+          <img src="./images/concept/intro02.jpg" alt="" class="pc"><img src="./images/concept/intro02-sp.jpg" alt="" class="sp">
+          <p class="annotation white">image photo</p>
+        </div>
+        </li>
+        <li>
+        <div class="image">
+          <img src="./images/concept/intro03.jpg" alt="" class="pc"><img src="./images/concept/intro03-sp.jpg" alt="" class="sp">
+          <p class="annotation white">image photo</p>
+        </div>
+        </li>
+      </ul>
+  </section><!-- intro -->
+
+
+
+  <section class="sections urban clearfix">
+    <div class="texts inner">
+      <p class="tit">URBAN CORE</p>
+      <div class="line">
+        <p class="txt01">名古屋の都心エリアを自在に楽しむ。</p>
+        <p class="txt02">ビジネス、カルチャー、ショッピング、レジャーなど、多彩なライフシーンを存分に謳歌する歓び。<br>名古屋の中枢エリアならではの輝きとトキメキを身近にして、<br class="pc">より優雅で充実したライフスタイルへの期待が高まる。</p>
+      </div>
+    </div>
+      <div class="image img01">
+        <img src="./images/concept/urban01.jpg" alt="" class="pc"><img src="./images/concept/urban01-sp.jpg" alt="" class="sp">
+        <p class="annotation">外観完成予想図・東別院（約340m／徒歩5分）</p>
+      </div>
+      <ul class="clearfix">
+        <li>
+        <div class="image">
+          <img src="./images/concept/urban02.jpg" alt="" class="pc"><img src="./images/concept/urban02-sp.jpg" alt="" class="sp">
+          <p class="annotation">東別院参道（約320ｍ／徒歩4分）</p>
+        </div>
+        </li>
+        <li>
+        <div class="image">
+          <img src="./images/concept/urban03.jpg" alt="" class="pc"><img src="./images/concept/urban03-sp.jpg" alt="" class="sp">
+          <p class="annotation white">下茶屋公園（約550m／徒歩7分）</p>
+        </div>
+        </li>
+      </ul>
+  </section>
+
+
+
+  <section class="sections calm clearfix">
+    <div class="texts inner">
+      <p class="tit">CALM ATMOSPHERE</p>
+      <div class="line">
+        <p class="txt01">都心にありながら、静穏の街に憩う。</p>
+        <p class="txt02">都心のアクティブモードから一転して、穏やかなプライベートモードへ。<br>四季の風景を奏でる寺社の参道や公園が心を和やかに癒してくれる街に、<br class="pc">快適で安らぎに満ちた日々がはじまる。</p>
+      </div>
+    </div>
+  </section>
+
+
+<div class="annotation-area">
+<p class="page-annotation js-scroll wrap clearfix">※掲載の外観完成予想図は、東別院境内から現地東側方向を撮影した写真（2023年6月撮影）に計画段階の図面を基に描き起こした建物完成予想図をCG合成・加工したもので、実際とは異なります。また、周辺環境・眺望は将来変わる場合があります。<br>※本物件の東側接近地の一部は、事業主・住友不動産株式会社の所有地であり、将来宅地開発等が行われる計画があります。（2023年地点において、この計画に関わる建築概要等は未定です。また、この計画を変更中止することがあります。）この計画が　実現しますと今回分譲する東側の住戸の眺望、通風等に影響が生じる場合がありますので、予めご了承ください。<br>※掲載の航空写真は金山駅付近上空から撮影（2023年6月）したものに光などCG合成・加工をしたものです。また、周辺環境は将来変わる場合があります。<br>※5駅6路線とは、地下鉄名城線「東別院」駅、地下鉄鶴舞線「大須観音」駅、地下鉄名城線・鶴舞線「上前津」駅、名鉄名古屋本線「山王」駅、JR東海道本線・中央線、名鉄名古屋本線、地下鉄名城線・名港線「金山」駅のことです。<br>※掲載の距離表示については地図上の概測距離を、徒歩分数表示については80ｍを1分として算出し、端数を切り上げたものです。<br>※掲載の写真は2023年3月、5月、6月撮影。<br>※掲載の情報は2023年7月現在のものです。</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
