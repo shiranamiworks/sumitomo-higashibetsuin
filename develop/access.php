bb11227b52fdb58f8a18c0df5374382889a8af1d
橋本真奈美 <manako.075h@gmail.com>
2023-09-12 18:21:34 +0900

    no message

---
+++ develop/access.php
@@ -0,0 +1,128 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "access",
+  "gnav_set" => "access",
+  "page_class" => "",
+  "url" => "",
+  "title" => "アクセス｜【公式】シティタワー東別院｜東別院の新築マンション｜住友不動産",
+  "keywords" => "アクセス,シティタワー東別院,東別院 マンション,東別院 新築マンション,東別院,東別院駅,地下鉄名城線,名古屋市中区,マンション,新築マンション,分譲マンション,住友不動産",
+  "description" => "アクセスページ。地下鉄名城線「東別院駅」まで徒歩8分。住友不動産の新築マンションが東別院に誕生。【すみふ東別院】名古屋市中区のマンションなら住友不動産。",
+  "add_stylesheet" => ["css/access.css","css/leaflet.css"],
+  "add_script" =>  [],
+    
+  "logo_text" => "東別院駅徒歩8分｜シティタワー東別院｜東別院 新築マンション｜アクセス｜すみふ東別院｜住友不動産"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー東別院｜東別院 マンション｜トップ</a></li>
+      <li><p>アクセス</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/access/mv.png" alt="" class="pc"><img src="./images/access/mv-sp.png" alt="" class="sp"><p class="annotation white">image photo</p></div>
+  </header><!-- main-visual -->
+
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <p class="lead">都心の主要エリアを掌中にする<br class="pc">快適なるアクセス拠点。
+      <p class="text">地下鉄名城線「東別院」駅をはじめ、徒歩圏で5駅6路線<sup>※1</sup>が利用可能。栄、伏見といったオフィス街や「名古屋」駅へのスムーズなアクセスを実現します。市内屈指のターミナル「金山」駅からは、名鉄名古屋本線で中部国際空港セントレアへ、JR東海道本線で三河方面へのアクセスも軽快。行動範囲がより快適に広がります。 </p>
+      <div class="image img01">
+        <img src="./images/access/img01.png" alt="">
+      </div>
+    </div>
+  </section><!-- intro -->
+
+
+
+  <section class="station wrap clearfix">
+    <ul class="lists list01 clearfix">
+      <li>
+      <div class="tit-box">
+        <p class="line">地下鉄名城線</p>
+        <p class="min">「<span class="name">東別院</span>」駅まで 徒歩<span class="num">8</span>分</p>
+      </div>
+      <div class="image">
+        <img src="./images/access/line01.jpg" alt="">
+        <p class="annotation white">image photo</p>
+      </div>
+      <div class="info-box">
+        <p class="min"><span class="name">「栄」駅</span> 直通<span class="big">6</span>分<span class="mini">（6分）</span></p>
+        <p class="sen">「東別院」駅より地下鉄名城線利用</p>
+      </div>
+      </li>
+      <li>
+      <div class="tit-box">
+        <p class="line">地下鉄舞鶴線</p>
+        <p class="min">「<span class="name">大須観音</span>」駅まで 徒歩<span class="num">12</span>分</p>
+      </div>
+      <div class="image">
+        <img src="./images/access/line02.jpg" alt="">
+        <p class="annotation white">image photo</p>
+      </div>
+      <div class="info-box">
+        <p class="min"><span class="name">「名古屋」駅</span> 直通<span class="big">14</span>分<span class="mini">（14分）</span></p>
+        <p class="sen">「東別院」駅より地下鉄名城線利用、<br>「栄」駅で地下鉄東山線に乗換え</p>
+      </div>
+      </li>
+    </ul><!-- list01 -->
+    <ul class="lists list02 clearfix">
+      <li>
+      <div class="image">
+        <img src="./images/access/line03.jpg" alt="">
+        <p class="annotation white">image photo</p>
+      </div>
+      <div class="info-box">
+        <p class="min"><span class="name">「伏見」駅</span> </p>
+        <p class="">直通<span class="big">1</span>分<span class="mini">（1分）</span></p>
+        <p class="sen">「大須観音」駅より地下鉄鶴舞線利用</p>
+      </div>
+      </li>
+      <li>
+      <div class="image">
+        <img src="./images/access/line04.jpg" alt="">
+        <p class="annotation white">image photo</p>
+      </div>
+      <div class="info-box">
+        <p class="min"><span class="name">「久屋大通」駅</span> </p>
+        <p class="">直通<span class="big">8</span>分<span class="mini">（8分）</span></p>
+        <p class="sen">「東別院」駅より地下鉄名城線利用</p>
+      </div>
+      </li>
+      <li>
+      <div class="image">
+        <img src="./images/access/line05.jpg" alt="">
+        <p class="annotation white">image photo</p>
+      </div>
+      <div class="info-box">
+        <p class="min"><span class="name">「名古屋城」駅</span> </p>
+        <p class="">直通<span class="big">10</span>分<span class="mini">（10分）</span></p>
+        <p class="sen">「東別院」駅より地下鉄名城線利用</p>
+      </div>
+      </li>
+    </ul><!-- list02 -->
+  </section><!-- station -->
+
+
+
+
+
+
+
+<div class="annotation-area">
+<p class="page-annotation js-scroll wrap clearfix">※距離表示については地図上の概測距離を、徒歩分数表示については80ｍを1分として算出し、端数を切り上げたものです。<br>※掲載の情報は2022年11月現在のものです。
+</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
