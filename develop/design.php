f60e99541d66e01d1d904ff43138ac3a70b5dd07
福井彩佳 <ayaka.fsp@gmail.com>
2024-08-02 17:40:43 +0900

    no message

---
+++ develop/design.php
@@ -0,0 +1,70 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "design",
+  "gnav_set" => "design",
+  "page_class" => "design",
+  "url" => "design.html",
+  "title" => "デザイン｜",
+  "keywords" => "デザイン,",
+  "description" => "デザインページ。",
+  "add_stylesheet" => ["css/design.css"],
+  "add_script" =>  [],
+  "canonical_page_id" => "design", 
+  "logo_text" => "デザイン｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー東別院｜東別院 マンション｜トップ</a></li>
+      <li><p>デザイン</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/design/mv.jpg" alt="" class="pc"><img src="./images/design/mv-sp.jpg" alt="" class="tb"><p class="annotation white">外観完成予想図</p></div>
+  </header><!-- main-visual -->
+
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <p class="title">街の景観に美しく際立つ洗練の邸。</p>
+      <p class="text">整然と連なるガラス手摺が描き出す印象的なファサード。<br>輝く光を纏った堂々たるフォルムが、新たな憧憬を創り出します。</p>
+      </div>
+      <div class="image img01">
+        <img src="./images/design/image01.jpg" alt=""><p class="annotation white">外観完成予想図・東別院（約340m／徒歩5分）</p>
+      </div>
+  </section><!-- intro -->
+
+
+
+  <section class="sections landplan clearfix">
+    <div class="texts inner">
+      <div class="tit"><img src="./images/design/title.svg" alt="" class="pc"></div>
+      <div class="line">
+        <p class="txt01">都市生活に快適さと安心感をもたらす、<br>全戸南東向きのランドスケープ。</p>
+        <p class="txt02">安心できる暮らしのために、日々をサポートする機能的な共用設備や仕様、さらに安全性に配慮した歩車分離設計を採用。<br>都心生活にふさわしいランドプランを実現しています。</p>
+      </div>
+      <div class="image img01">
+        <img src="./images/design/landplan.png" alt=""><p class="annotation">敷地配置イラスト</p>
+      </div>
+    </div>
+  </section>
+
+
+
+<div class="annotation-area">
+<p class="page-annotation js-scroll wrap clearfix">※掲載の完成予想図は計画段階の図面を基に描き起こしたものに人物等をCG合成したもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。<br>
+※掲載の外観完成予想図は、東別院境内から現地東側方向を撮影した写真（2023年6月撮影）に計画段階の図面を基に描き起こした建物完成予想図をCG合成・加工したもので、実際とは異なります。　また、周辺環境・眺望は将来変わる場合があります。また、形状の細部および設備機器等については省略しております。<br>
+※掲載の敷地配置イラストは計画段階の図面を基に描き起こしたもので、形状・色等は実際とは多少異なります。また、一部敷地外の道路等を合わせて着彩しています。</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
---
+++ develop/public.php
@@ -0,0 +1,112 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "public",
+  "gnav_set" => "public",
+  "page_class" => "public",
+  "url" => "public.html",
+  "title" => "共用空間｜",
+  "keywords" => "コンセプト,",
+  "description" => "コンセプトページ。",
+  "add_stylesheet" => ["css/public.css"],
+  "add_script" =>  [],
+  "canonical_page_id" => "public", 
+  "logo_text" => "コンセプト｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー東別院｜東別院 マンション｜トップ</a></li>
+      <li><p>コンセプト</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/public/mv.jpg" alt="" class="pc"><img src="./images/public/mv-sp.jpg" alt="" class="tb"><p class="annotation white">航空写真</p></div>
+  </header><!-- main-visual -->
+
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <p class="text">再開発で進化を続ける名古屋の3大エリア、<br class="pc">名駅、栄、金山の中心に誕生する、シティタワー東別院。<br>地下鉄名城線「東別院」駅をはじめ5駅6路線<sup>※</sup>を徒歩圏にするポジションが<br class="pc">多彩な利便と快適性を演出し、都心にありながら<br class="pc">歴史の面影や緑に彩られたロケーションが暮らしに寛ぎと潤いを届ける。<br>先進の都市性と安らぎが調和する街に際立つ、<br class="pc">地上20階建て全175邸の格調ある造形が街の新しいランドマークとなって<br class="pc">住まう方の未来と可能性を豊かに広げていく。</p>
+      </div>
+      <div class="image img01">
+        <img src="./images/public/intro01.jpg" alt="" class="pc"><img src="./images/public/intro01-sp.jpg" alt="" class="tb">
+        <p class="annotation white">image photo</p>
+      </div>
+      <ul class="clearfix">
+        <li>
+        <div class="image">
+          <img src="./images/public/intro02.jpg" alt="" class="pc"><img src="./images/public/intro02-sp.jpg" alt="" class="tb">
+          <p class="annotation white">image photo</p>
+        </div>
+        </li>
+        <li>
+        <div class="image">
+          <img src="./images/public/intro03.jpg" alt="" class="pc"><img src="./images/public/intro03-sp.jpg" alt="" class="tb">
+          <p class="annotation white">image photo</p>
+        </div>
+        </li>
+      </ul>
+  </section><!-- intro -->
+
+
+
+  <section class="sections urban clearfix">
+    <div class="texts inner">
+      <p class="tit">URBAN CORE</p>
+      <div class="line">
+        <p class="txt01">名古屋の都心エリアを自在に楽しむ。</p>
+        <p class="txt02">ビジネス、カルチャー、ショッピング、レジャーなど、多彩なライフシーンを存分に謳歌する歓び。<br>名古屋の中枢エリアならではの輝きとトキメキを身近にして、<br class="pc">より優雅で充実したライフスタイルへの期待が高まる。</p>
+      </div>
+      <div class="btn"><a href="./location01.html">詳細はこちら ></a></div>
+    </div>
+      <div class="image img01">
+        <img src="./images/public/urban01.jpg" alt="" class="pc"><img src="./images/public/urban01-sp.jpg" alt="" class="tb">
+        <p class="annotation">外観完成予想図・東別院（約340m／徒歩5分）</p>
+      </div>
+      <ul class="clearfix">
+        <li>
+        <div class="image">
+          <img src="./images/public/urban02.jpg" alt="" class="pc"><img src="./images/public/urban02-sp.jpg" alt="" class="tb">
+          <p class="annotation">東別院参道（約320ｍ／徒歩4分）</p>
+        </div>
+        </li>
+        <li>
+        <div class="image">
+          <img src="./images/public/urban03.jpg" alt="" class="pc"><img src="./images/public/urban03-sp.jpg" alt="" class="tb">
+          <p class="annotation white">下茶屋公園（約550m／徒歩7分）</p>
+        </div>
+        </li>
+      </ul>
+  </section>
+
+
+
+  <section class="sections calm clearfix">
+    <div class="texts inner">
+      <p class="tit">CALM ATMOSPHERE</p>
+      <div class="line">
+        <p class="txt01">都心にありながら、静穏の街に憩う。</p>
+        <p class="txt02">都心のアクティブモードから一転して、穏やかなプライベートモードへ。<br>四季の風景を奏でる寺社の参道や公園が心を和やかに癒してくれる街で、<br class="pc">快適で安らぎに満ちた日々がはじまる。</p>
+      </div>
+      <div class="btn"><a href="./location02.html">詳細はこちら ></a></div>
+    </div>
+  </section>
+
+
+<div class="annotation-area">
+<p class="page-annotation js-scroll wrap clearfix">※掲載の外観完成予想図は、東別院境内から現地東側方向を撮影した写真（2023年6月撮影）に計画段階の図面を基に描き起こした建物完成予想図をCG合成・加工したもので、実際とは異なります。
+また、周辺環境・眺望は将来変わる場合があります。また、形状の細部および設備機器等については省略しております。
+<br>※本物件の東側隣接地の一部は、事業主・住友不動産株式会社の所有地であり、将来宅地開発等が行われる計画があります。（2023年時点において、この計画に関わる建築概要等は未定です。また、この計画を変更中止することがあります。）この計画が実現しますと今回分譲する東側の住戸の眺望、通風等に影響が生じる場合がありますので、予めご了承ください。<br>※掲載の航空写真は金山駅付近上空から撮影（2023年6月）したものに光などCG合成・加工をしたものです。また、周辺環境は将来変わる場合があります。<br>※5駅6路線とは、地下鉄名城線「東別院」駅、地下鉄鶴舞線「大須観音」駅、地下鉄名城線・鶴舞線「上前津」駅、名鉄名古屋本線「山王」駅、JR東海道本線・中央線、名鉄名古屋本線、地下鉄名城線・名港線「金山」駅のことです。<br>※掲載の距離表示については地図上の概測距離を、徒歩分数表示については80ｍを1分として算出し、端数を切り上げたものです。<br>※掲載の写真は2023年3月、5月、6月撮影。<br>※掲載の情報は2023年7月現在のものです。</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
