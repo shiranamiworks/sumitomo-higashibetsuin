b290b107b918c259bfe8c0fe598b7140b33b18d6
福井彩佳 <ayaka.fsp@gmail.com>
2024-09-12 18:40:09 +0900

    no message

---
+++ develop/js/private.js
@@ -0,0 +1,19 @@
+$(function () {
+    $(".slider").slick({
+      autoplay: true,
+      arrows: true,
+      fade: true,
+      asNavFor: ".thumbnail",
+    });
+    $(".thumbnail").slick({
+      slidesToShow: 8,
+      asNavFor: ".slider",
+      focusOnSelect: true,
+          responsive: [
+              {
+                  breakpoint: 768,
+                  slidesToShow: 3,
+              }
+          ]
+    });
+  });
---
+++ develop/private.php
@@ -0,0 +1,127 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "private",
+  "gnav_set" => "private",
+  "page_class" => "private",
+  "url" => "private.html",
+  "title" => "室内空間｜",
+  "keywords" => "室内空間,",
+  "description" => "室内空間ページ。",
+  "add_stylesheet" => ["css/private.css"],
+  "add_script" =>  ["js/private.js"],
+  "canonical_page_id" => "private", 
+  "logo_text" => "室内空間｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー東別院｜東別院 マンション｜トップ</a></li>
+      <li><p>室内空間</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+<div class="content">
+  <div class="slider">
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img01.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img02.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img03.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img04.jpg" alt="">
+        <p class="caption">DINING ROOM</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img05.jpg" alt="">
+        <p class="caption">KITCHEN</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img06.jpg" alt="">
+        <p class="caption">BALCONY</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img07.jpg" alt="">
+        <p class="caption">POWDER ROOM</p>
+        <p class="caption right">BATHROOM</p>
+      </div>
+    </div>
+  </div>
+  <div class="thumbnail">
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img01.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img02.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img03.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img04.jpg" alt="">
+        <p class="caption">DINING ROOM</p>
+      </div>
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img05.jpg" alt="">
+        <p class="caption">KITCHEN</p>
+      </div>    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img06.jpg" alt="">
+        <p class="caption">BALCONY</p>
+      </div>    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img07.jpg" alt="">
+        <p class="caption">POWDER ROOM<br>BATHROOM</p>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+
+<div class="annotation-area">
+<p class="page-annotation js-scroll wrap clearfix">※掲載の写真はモデルルーム（Dcタイプ）を撮影（2024年8月）したもので、家具・調度品等オプション仕様は販売価格に含まれておりません。<br>※バルコニーの家具・植栽・調度品等の配置はイメージです。実際に配置される際には、安全等に十分ご配慮ください。<br>※モデルの仕様は、施工上の都合・改良により一部変更になる場合があります。</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
