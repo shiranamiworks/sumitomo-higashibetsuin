4b68815602753be32c76299410cba7ea74fed420
福井彩佳 <ayaka.fsp@gmail.com>
2024-09-27 11:15:11 +0900

    no message

--- develop/sitemap.php
+++ develop/sitemap.php
@@ -28,9 +28,9 @@
 						<li class="private"><a href="private.html">室内空間</a></li>
 						<li class="plan"><a href="https://www.sumitomo-rd-mansion.jp/tokai/higashibetsuin/list.php" target="_blank">間取り</a></li>
 						<li class="equipment"><span>設備・仕様<br />
-						（ <a href="equipment_01.html">キッチン</a> ／ <a href="equipment_02.html">サニタリー</a> ／ <a href="equipment_03.html">健康・エコ・省エネ</a> ／ <a href="equipment_04.html">収納・快適仕様</a> ／ <a href="equipment_05.html">ユニバーサルデザイン・マルチメディア</a> ）</span></li>
+						（ <a href="equipment01.html">キッチン・サニタリー</a> ／ <a href="equipment02.html">健康・エコ・省エネ</a> ／ <a href="equipment03.html">収納</a> ／ <a href="equipment04.html">マルチメディア</a> ／ <a href="equipment05.html">快適提案</a> ）</span></li>
 						<li class="structure"><span>構造・防災<br />
-						（ <a href="structure_01.html">基本構造</a> ／ <a href="structure_02.html">セキュリティ</a> ／ <a href="structure_03.html">防災対策</a> ）</span></li>
+						（ <a href="structure01.html">基本構造</a> ／ <a href="structure02.html">災害対策装備</a> ／ <a href="structure03.html">セキュリティ</a> ）</span></li>
 						<li class="location"><span>ロケーション
 						（ <a href="location01.html">進化する都心エリア</a> ／ <a href="location02.html">静穏な邸宅地</a> ）</span></li>
 						<li class="access"><a href="access.html">アクセス</a></li>
