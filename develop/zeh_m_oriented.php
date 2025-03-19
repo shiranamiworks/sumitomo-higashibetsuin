8deadc911baf9cf38fc797ca8be988db844539fc
福井彩佳 <ayaka.fsp@gmail.com>
2025-03-19 23:53:08 +0900

    no message

---
+++ develop/zeh_m_oriented.php
@@ -0,0 +1,164 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "zeh_m_oriented",
+  "gnav_set" => "zeh_m_oriented",
+  "page_class" => "zeh_m_oriented",
+  "url" => "zeh_m_oriented.html",
+  "title" => "ZEH-M Oriented｜",
+  "keywords" => "ZEH-M Oriented,",
+  "description" => "ZEH-M Orientedページ。",
+  "add_stylesheet" => ["css/zeh_m_oriented.css","css/animate.css","css/equipment.css"],
+  "add_script" =>  ["js/delighters.min.js"],
+    
+  "logo_text" => "ZEH-M Oriented｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー名古屋東別院｜東別院 マンション｜トップ</a></li>
+      <li><a href="./equipment01.html">設備・仕様</a></li>
+      <li><p>ZEH-M Oriented</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <div class="equipment-link equipment-link--5">
+    <a href="equipment01.html"><p>キッチン・サニタリー</p></a>
+    <a href="equipment02.html"><p>健康・エコ・省エネ</p></a>
+    <a href="equipment03.html"><p>収納</p></a>
+    <a href="equipment04.html"><p>マルチメディア</p></a>
+    <a href="equipment05.html"><p>快適提案</p></a>
+    <a href="equipment05.html" class="current"><p>ZEH-M Oriented</p></a>
+  </div>
+
+  <!-- contents// -->
+  <div id="contents">
+			<div class="inner">
+        
+        <!-- //共通エリア -->
+        <div id="mainImage">
+          <h2>
+            <img class="pc" src="images/zeh_m/mainimg.jpg" alt="ZEH-M Oriented">
+            <img class="sp" src="images/zeh_m/mainimg_sp.jpg" alt="ZEH-M Oriented">
+          </h2>
+        </div>
+        
+        <div id="about" data-ani="fade-up" data-delighter>
+          <p>「ZEH」は、ネット・ゼロ・エネルギー・ハウスの略であり、<br>外壁など外皮の断熱性能等を向上させ、高効率な設備システムなどを導入し、<br>「省エネ」を実現した上で、年間の一次エネルギー消費量の収支をゼロとすることを目指した住宅です。<br>また、一次エネルギー消費量の削減する段階毎に基準が設定されました。<br>当社が標準化する「ZEH－M Oriented」は、強化外皮基準（断熱性能）を満たし、<br>省エネによる一次エネルギー消費量を現行の省エネ基準値から20％以上削減とする基準です。</p>
+        </div>
+        
+        <div id="definition">
+          <div class="housing" data-ani="fade-up" data-delighter>
+						<h4><span>集合住宅における</span><span>ZEH-M Orientedの定義</span></h4>
+						
+						<p class="tableTitle">■住棟での評価</p>
+						<table>
+							<thead>
+								<tr>
+									<th>外皮断熱性能</th>
+									<th>省エネ率<sup>※1</sup></th>
+									<th>目指すべき水準</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><span>強化外皮基準</span><span>（ZEH基準）</span></td>
+									<td>20％以上</td>
+									<td>6階建て以上</td>
+								</tr>
+							</tbody>
+						</table>
+						<p class="caption">出典：集合住宅におけるZEHロードマップフォローアップ委員会「集合住宅におけるZEHの設計ガイドライン」</p>
+						<p class="caption">※1：再生エネルギーを除く</p>
+					</div>
+          
+          <div class="factorWrap">
+            <div class="factorItem">
+              <div class="factor" id="energy1" data-ani="fade-right" data-delighter>
+                <p class="factorTitle">断熱性の大幅な向上</p>
+                <p class="factorCopy"><span>■</span>エネルギーを極力必要としない</p>
+
+                <div class="factorImg">
+                  <div class="imgBox">
+                    <img class="base" src="images/zeh_m/energy1_sunlight.svg" alt="">
+                    <img class="puton" src="images/zeh_m/energy1_mansion.png" alt="image">
+                    <p class="imgCaption">Image</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="factorItem">
+              <div class="factor" id="energy2" data-ani="fade-left" data-delighter>
+                <p class="factorTitle">省エネ設備の導入</p>
+                <p class="factorCopy"><span>■</span>エネルギーを上手に使う</p>
+
+                <div class="factorImg">
+                  <div class="imgBox">
+                    <img src="images/zeh_m/energy2.png" alt="image">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="summary">
+            <div class="txtBox" data-ani="fade-up" data-delighter>
+              <p><span class="ibspan">「断熱性の向上」と</span><span class="ibspan">「省エネ設備の導入」により</span><br><span class="ibspan">住棟全体の年間一次エネルギー消費量を</span><span class="ibspan">現行省エネ基準値から</span></p>
+            </div>
+            
+            <h3 data-ani="fade-up" data-delighter>20%<span class="harfsize">以上</span>削減<span class="harfsize">する</span><br><span class="ibspan">ZEH-M Orieted</span><span class="ibspan">マンション</span></h3>
+          </div>
+          
+        </div>
+        <!-- //共通エリア -->
+        
+        <!-- 物件個別エリア -->
+        <div id="bels" data-ani="fade-up" data-delighter>
+          <div id="belsTitle">
+            <img class="pc" src="images/zeh_m/belstitle.png" alt="省エネルギー性能を評価する[BELS（ベルス）]取得">
+            <img class="sp" src="images/zeh_m/belstitle_sp.png" alt="省エネルギー性能を評価する[BELS（ベルス）]取得">
+          </div>
+          <div id="belsContents">
+            <div class="belsbody">
+              <div class="boxleft">
+                <p class="belscopy">BELS（ベルス）とは、建築物省エネルギー性能表示制度のことで、全ての建築物において、省エネ性能を第三者評価機関が評価し認定する制度です。「（物件名）シティ○○○○」は、○つ星を取得し、省エネ基準比○○％のエネルギー消費量削減及び評価を取得しました。</p>
+              </div>
+              <div class="boxright">
+                <img src="images/zeh_m/belsmark.png" alt="BELS">
+              </div>
+            </div>
+            
+            <div class="belscaption">
+              <p class="caption">※本表示は「住棟全体」の評価であり、「各住戸」の性能を担保するものではありません。<br>また、住戸プラン変更等により変更が生じる場合があります。</p>
+            </div>
+          </div>
+        </div>
+        <!-- //物件個別エリア -->
+        
+			</div>
+			
+			<!-- pagetop// -->
+			<!-- <div id="pagetop"><a href="#head"><img src="images/common/page_top.gif" alt="Page Top"></a></div> -->
+			<!-- //pagetop -->
+			
+		</div>
+		<!-- //contents -->
+  <div class="equipment-link equipment-link--5">
+    <a href="equipment01.html"><p>キッチン・サニタリー</p></a>
+    <a href="equipment02.html"><p>健康・エコ・省エネ</p></a>
+    <a href="equipment03.html"><p>収納</p></a>
+    <a href="equipment04.html"><p>マルチメディア</p></a>
+    <a href="equipment05.html"><p>快適提案</p></a>
+    <a href="equipment05.html" class="current"><p>ZEH-M Oriented</p></a>
+  </div>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
